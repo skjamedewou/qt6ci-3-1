dde7765adee362a65f1185fcebae906e87815677
SergeA <amedewous@gmail.com>
2025-08-27 14:49:54 +0200

    add test

---
+++ test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <QObject>
+
+class Test : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Test(QObject *parent = nullptr);
+
+signals:
+};
+
+#endif // TEST_H
